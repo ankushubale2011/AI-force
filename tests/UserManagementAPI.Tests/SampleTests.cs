a21cab4c531fea21d9dbc3d05ad80f708a277ab6
ankushubale2011 <61446675+ankushubale2011@users.noreply.github.com>
2026-02-24 16:47:20 +0530

    test: add basic sample test

---
+++ tests/UserManagementAPI.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UserManagementAPI.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void BasicMath_Works()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
